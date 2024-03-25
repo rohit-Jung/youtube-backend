d3ed4af044f5b11432daf5c945cb16958c6348e5
rohit-Jung <rokshh28@gmail.com>
2024-03-25 18:13:21 +0545

    updated the healthcheck Controller

--- src/controllers/healthcheck.controller.js
+++ src/controllers/healthcheck.controller.js
@@ -3,7 +3,21 @@ import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 const healthcheck = asyncHandler(async (req, res) => {
-    //TODO: build a healthcheck response that simply returns the OK status as json with a message
+    try {
+        // Build healthcheck response
+        const healthcheckResponse = new ApiResponse(
+            200,
+            "OK",
+            "Healthcheck successful"
+        );
+        // Return healthcheck response
+        res.status(200).json(healthcheckResponse);
+    } catch (error) {
+        // Handle errors
+        console.error(error);
+        const apiError = new ApiError(500, "Internal server error");
+        res.status(apiError.statusCode).json(apiError);
+    }
 });
 
 export { healthcheck };
